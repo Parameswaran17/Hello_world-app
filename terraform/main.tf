21fe99b7c80d46e1a19fef96db48fa757c483a90
Parameswaran17 <158277128+Parameswaran17@users.noreply.github.com>
2024-05-17 13:02:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,8 @@ resource "aws_instance" "ecs_instance" {
       type        = "ssh"
       user        = "ec2-user"  # SSH user for the instance
       host        = self.public_ip  # Public IP address of the instance
-      private_key = file("C:\Users\HP\.ssh\id_rsa")  # Specify your SSH private key
-     # Passphrase for the private key
-      passphrase = ""
+      private_key = file("C:/Users/HP/.ppk/key.ppk")  # Specify your SSH private key
+      agent       = true # Use SSH agent (Pageant)
     }
   }
 }

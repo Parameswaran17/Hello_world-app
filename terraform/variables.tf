1db38c17df950c2ac706ca87c49a66ec51a812c7
Parameswaran17 <158277128+Parameswaran17@users.noreply.github.com>
2024-05-17 15:20:40 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,44 +1,59 @@
-variable "vpc_id" {
-  description = "The ID of the VPC where resources will be deployed."
+variable "aws_region" {
+  description = "The AWS region to deploy to"
   type        = string
+  default     = "us-east-1"
 }
 
-variable "subnets" {
-  description = "A list of subnet IDs where resources will be deployed."
-  type        = list(string)
-}
-
-variable "security_groups" {
-  description = "A list of security group IDs for the ECS tasks."
-  type        = list(string)
-}
-
-variable "container_definitions" {
-  description = "JSON encoded container definitions for ECS task."
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "ecs_instance_type" {
-  description = "EC2 instance type for ECS tasks."
-  default     = "t2.micro"
-}
-
-variable "ecs_desired_count" {
-  description = "Number of ECS tasks to run."
-}
-
-variable "ecs_min_capacity" {
-  description = "Minimum number of ECS tasks to run."
-}
-
-variable "ecs_max_capacity" {
-  description = "Maximum number of ECS tasks to run."
+variable "subnet_cidrs" {
+  description = "List of CIDR blocks for the subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "container_port" {
-  description = "Port on which the Docker container listens."
+  description = "The port on which the container will listen"
+  type        = number
+  default     = 3000
 }
 
-variable "lb_port" {
-  description = "Port on which the load balancer listens."
+variable "desired_count" {
+  description = "The desired number of ECS service tasks"
+  type        = number
+  default     = 1
+}
+
+variable "task_memory" {
+  description = "The amount of memory (in MiB) to allocate for the ECS task"
+  type        = string
+  default     = "512"
+}
+
+variable "task_cpu" {
+  description = "The amount of CPU units to allocate for the ECS task"
+  type        = string
+  default     = "256"
+}
+
+variable "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+  default     = "hello-world-cluster"
+}
+
+variable "ecs_service_name" {
+  description = "The name of the ECS service"
+  type        = string
+  default     = "hello-world-service"
+}
+
+variable "ecr_repository_name" {
+  description = "The name of the ECR repository"
+  type        = string
+  default     = "hello-world-repo"
 }
